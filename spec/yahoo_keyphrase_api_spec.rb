c5a7cd91f3f27d3c5ba167328cf9d6eeb3eb352d
kyohei8 <tsukuda.kyouhei@gmail.com>
2013-10-07 22:46:01 +0900

    mod test code

--- spec/yahoo_keyphrase_api_spec.rb
+++ spec/yahoo_keyphrase_api_spec.rb
@@ -6,7 +6,9 @@ describe 'YahooKeyphraseApi' do
 
   before do
     tokens = YAML.load_file(File.join(File.dirname(__FILE__),'./application.yml'))["test"]
+    p tokens
     YahooKeyphraseApi::Config.app_id = tokens['app_id']
+    p YahooKeyphraseApi::Config.app_id
     @ykp = YahooKeyphraseApi::KeyPhrase.new
   end
 
@@ -16,6 +18,7 @@ describe 'YahooKeyphraseApi' do
 
   it 'should get keyphrases case POST' do
     res =  @ykp.extract('東京ミッドタウンから国立新美術館まで歩いて5分で着きます。のリクエストに対するレスポンスです。')
+    p res
     res.should_not == nil
   end
 
@@ -42,7 +45,7 @@ describe 'YahooKeyphraseApi' do
     large_file = File.dirname(__FILE__) + '/LargeData.txt'
     p "#{(FileTest.size?(large_file)/1024)}KB" # its 33KB! why error? :(
     c = File.read(large_file, encding:Encoding::UTF_8)
-
+    p c
     lambda{
       @ykp.extract(c)
     }.should raise_error(YahooKeyphraseApi::YahooKeyPhraseApiError, 'Request Entity Too Large')
